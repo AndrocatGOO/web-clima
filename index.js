4cdb4e4f43cb6a7b4cb55b5a0ac5ff058afe8579
AndrocatGOO <andresfabiangarzonvalencia@gmail.com>
2021-07-01 07:26:03 -0500

    a
    a

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const APY_CLIMA_KEY ="05d15a22fc70a03fd6b58ce55c838b91";
+const APY_CLIMA_KEY = "05d15a22fc70a03fd6b58ce55c838b91";
 
-const fetchData = position =>{
-    const {latitude, longitude}= position.coords;
+const fetchData = position => {
+    const { latitude, longitude } = position.coords;
     fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=${APY_CLIMA_KEY}`)
-        .then(response => response.json())
-        .then(data => setWeatherData(data))
+    .then(response => response.json())
+    .then(data => setWeatherData(data))
 };
 
-const setWeatherData = data =>{
+const setWeatherData = data => {
     console.log(data);
     const weatherData = {
-        location:data.name,
-        description:data.weather[0].main,
-        humedad:data.main.humidity,
-        presion:data.main.pressure,
-        temperatura:data.main.temp,
-        date:getDate(),
+        location: data.name,
+        description: data.weather[0].main,
+        humedad: data.main.humidity,
+        presion: data.main.pressure,
+        temperatura: data.main.temp,
+        date: getDate(),
     }
-    Object.keys(weatherData).forEach ( key =>{
+    Object.keys(weatherData).forEach(key => {
         document.getElementById(key).textContent = weatherData[key];
     });
 };
 
-const getDate = () =>{
+const getDate = () 
+=>   
+{
     let date = new Date();
-    return `${date.getDate()}-${date.getMonth()+1}-${date.getYear()+1900}`;
+    return `${date.getDate()}-${date.getMonth() + 1}-${date.getYear() + 1900}`;
 }
 
 
-const onload = () =>{
+const onload = () => {
     navigator.geolocation.getCurrentPosition(fetchData);
 };
